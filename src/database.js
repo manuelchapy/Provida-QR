7b82cde6d35d0097c44fb061b9337838beb67264
unknown <manuelchaparro94@gmail.com>
2022-12-31 14:27:37 -0400

    prueba sin DB

--- src/database.js
+++ src/database.js
@@ -1,13 +1,16 @@
 //require('dotenv').config();
 const {connect} = require('mongoose');
 //import connect from "mongoose";
-
+/*
 //funcion anonima
 (async () => {
     try {
-        const db = await connect("mongodb+srv://Chapy:chapybass@providadb.evwbf.mongodb.net/ProVidaDB?retryWrites=true&w=majority")
+        //mongodb://localhost/providaQR
+        //mongodb+srv://Chapy:chapybass@providadb.evwbf.mongodb.net/ProVidaDB?retryWrites=true&w=majority
+        const db = await connect("mongodb://localhost/providaQR")
         console.log('DB connected', db.connection.name)
     } catch (error) {
         console.error(error)
     }
 })()
+*/
