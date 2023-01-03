c910d54ce71e2bf8e107a630de08260fdf6d4126
unknown <manuelchaparro94@gmail.com>
2023-01-03 11:47:44 -0400

    required delete

--- models/clientes.js
+++ models/clientes.js
@@ -2,15 +2,15 @@ const {Schema, model} = require('mongoose');
 //const moment = require('moment-timezone');
 
 const clienteSchema = new Schema({
-	idCliente: 			{type: Number, required: true},
-	clienteNombre:   	{type: String, required: true},
-	clienteApellido:   	{type: String, required: true},
-	cedula_RIF: 		{type: String, required: true, unique: true},
+	idCliente: 			{type: Number},
+	clienteNombre:   	{type: String},
+	clienteApellido:   	{type: String},
+	cedula_RIF: 		{type: String},
 	contacto: 			{type: String},
-	correo: 			{type: String, required: true},
-	telefono: 			{type: String, required: true},
-	tipoCliente: 		{type: String, required: true},
-    qr:                 {type: String, required: true},
+	correo: 			{type: String},
+	telefono: 			{type: String},
+	tipoCliente: 		{type: String},
+    qr:                 {type: String},
 });
 
 clientes = model('clientes_qrs', clienteSchema, 'clientes_qrs');
